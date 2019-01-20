d2c24518dc9bbddd8139289f5aacaf05e0eca5f3
TLROJE787 <38369651+TLROJE787@users.noreply.github.com>
2019-01-20 17:41:19 +0800

    Add files via upload

---
+++ objlist.js
@@ -0,0 +1,57 @@
+    //row object list
+    var row1 = [
+        {herf:'./index.html',name:'登录/注册'},
+        {herf:'./ren_yuan_guan_li.html',name:'用户权限管理'},
+        {herf:'./xuan_ti_guan_li.html',name:'选题环节管理'},
+        {herf:'./chou_gao_guan_li.html',name:'稿酬信息管理'},
+        {herf:'./he_tong_guan_li.html',name:'合同模板管理'},
+        {herf:'./fa_gao_guan_li.html',name:'发稿信息管理'},
+        {herf:'./yin_zhi_guan_li.html',name:'印制版式管理'},
+        {herf:'./fa_xing_guan_li.html',name:'发行量管理'},
+        {herf:'./ku_cun_guan_li.html',name:'库存信息管理'},
+        ]
+        var row2 = [
+            {herf:'./index.html',name:'修改账号信息'},
+            {herf:'./ren_yuan_guan_li.html',name:'个人信息管理'},
+            {herf:'./xuan_ti_guan_li.html',name:'选题策略管理'},
+            {herf:'./chou_gao_guan_li.html',name:'稿酬计算'},
+            {herf:'./he_tong_guan_li.html',name:'合同信息管理'},
+            {herf:'./fa_gao_guan_li.html',name:'政策法规管理'},
+            {herf:'./yin_zhi_guan_li.html',name:'印制信息管理'},
+            {herf:'./fa_xing_guan_li.html',name:'预售书籍'},
+            {herf:'./ku_cun_guan_li.html',name:'出库信息管理'},
+            ]
+            var row3 = [
+                {herf:'./index.html',name:'增加删除账号'},
+                {herf:'',name:'        '},
+                {herf:'./xuan_ti_guan_li.html',name:'约稿管理'},
+                {herf:'',name:'        '},
+                {herf:'',name:'        '},
+                {herf:'./fa_gao_guan_li.html',name:'发稿审核'},
+                {herf:'',name:'        '},
+                {herf:'./fa_xing_guan_li.html',name:'发布消息'},
+                {herf:'./ku_cun_guan_li.html',name:'入库信息管理'},
+                ]
+                var row4 = [
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'',name:'        '},
+                    {herf:'./fa_xing_guan_li.html',name:'统计销量'},
+                    {herf:'',name:'        '},
+                    ]
+                    var row5 = [
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'',name:'        '},
+                        {herf:'./fa_xing_guan_li.html',name:'促销策略管理'},
+                        {herf:'',name:'        '},
+                        ]
+var rowList=[row1,row2,row3,row4,row5];
